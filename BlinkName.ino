1679e7033f42cd552435e59db3dbc486810b0ff6
omsenpaiii <123073338+omsenpaiii@users.noreply.github.com>
2023-08-22 10:04:16 +0530

    Update BlinkName.ino

--- BlinkName.ino
+++ BlinkName.ino
@@ -1,142 +1,70 @@
-// SIT210 2.1P
-int led = LED_BUILTIN;
-int buttonPin = 2; // Change this to the pin where your button is connected
-bool buttonState = false;
-bool lastButtonState = false;
+int led1 = LED_BUILTIN;
+
+int buttonPin = 2; // Push button for resetting the name
+bool isButtonPressed = false;
+bool isBlinking = true; // Flag to control blinking state
+unsigned long lastButtonPressTime = 0; // To track the time of the last button press
 
 void setup() {
-  pinMode(led, OUTPUT);
+  pinMode(led1, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);
-  Serial.begin(9600);
 }
 
-void loop() {
-  buttonState = digitalRead(buttonPin);
+void loop() 
+{
+  // Check for external reset button press
+  checkButtonPress();
 
-  if (buttonState == LOW && lastButtonState == HIGH) {
-    // Button was pressed
-    String name = readNameFromSerial();
-    blinkNameInMorse(name);
-  }
-
-  lastButtonState = buttonState;
-}
-
-String readNameFromSerial() {
-  String inputName = "";
-  while (Serial.available() > 0) {
-    char c = Serial.read();
-    Serial.println(c);
-    if (c == '\n') {
-      break;
+  if (isButtonPressed) {
+    unsigned long currentTime = millis();
+    // Check if it's been more than 1 second since the last button press
+    if (currentTime - lastButtonPressTime >= 1000) {
+      resetName();
+      isButtonPressed = false;
+      isBlinking = true; // Enable blinking after resetting
+      lastButtonPressTime = currentTime;
     }
-    inputName += c;
   }
-  return inputName;
+
+  if (isBlinking) {
+    // Your name in Morse code
+    blinkDash(); delay(1000); // Blink 'O' dash
+    blinkDash(); delay(3000); // Gap between letters
+
+    blinkDash(); delay(1000); // Blink 'M' dash
+    blinkDash(); delay(1000);
+    blinkDash(); delay(1000);
+
+    delay(3000); // Gap between words
+  }
 }
 
 void blinkDot() {
-  digitalWrite(led, HIGH);
-  delay(300);
-  digitalWrite(led, LOW);
-  delay(300);
+  digitalWrite(led1, HIGH);
+  delay(200);
+  digitalWrite(led1, LOW);
+  delay(200);
 }
 
 void blinkDash() {
-  digitalWrite(led, HIGH);
-  delay(900);
-  digitalWrite(led, LOW);
-  delay(300);
+  digitalWrite(led1, HIGH);
+  delay(600);
+  digitalWrite(led1, LOW);
+  delay(200);
 }
 
-void blinkNameInMorse(String name) {
-  for (int i = 0; i < name.length(); i++) {
-    char c = tolower(name[i]);
-    switch (c) {
-      case 'a':
-        blinkDot(); delay(600); blinkDash(); delay(900);
-        break;
-      case 'b':
-        blinkDash(); delay(600); blinkDot(); delay(600); blinkDot(); delay(600); blinkDot(); delay(600);
-        break;
-      case 'c':
-        blinkDash(); delay(600); blinkDot(); delay(600); blinkDash(); delay(600); blinkDot(); delay(600);
-        break;
-      case 'd':
-        blinkDash(); delay(600); blinkDot(); delay(600); blinkDot(); delay(600);
-        break;
-      case 'e':
-        blinkDot(); delay(600);
-        break;
-      case 'f':
-        blinkDot(); delay(600); blinkDot(); delay(600); blinkDash(); delay(600); blinkDot(); delay(600);
-        break;
-      case 'g':
-        blinkDash(); delay(600); blinkDash(); delay(600); blinkDot(); delay(600);
-        break;
-      case 'h':
-        blinkDot(); delay(600); blinkDot(); delay(600); blinkDot(); delay(600); blinkDot(); delay(600);
-        break;
-      case 'i':
-        blinkDot(); delay(600); blinkDot(); delay(600);
-        break;
-      case 'j':
-        blinkDot(); delay(600); blinkDash(); delay(600); blinkDash(); delay(600); blinkDash(); delay(600);
-        break;
-      case 'k':
-        blinkDash(); delay(600); blinkDot(); delay(600); blinkDash(); delay(600);
-        break;
-      case 'l':
-        blinkDot(); delay(600); blinkDash(); delay(600); blinkDot(); delay(600); blinkDot(); delay(600);
-        break;
-      case 'm':
-        blinkDash(); delay(600); blinkDash(); delay(600);
-        break;
-      case 'n':
-        blinkDash(); delay(600); blinkDot(); delay(600);
-        break;
-      case 'o':
-        blinkDash(); delay(600); blinkDash(); delay(600); blinkDash(); delay(600);
-        break;
-      case 'p':
-        blinkDot(); delay(600); blinkDash(); delay(600); blinkDash(); delay(600); blinkDot(); delay(600);
-        break;
-      case 'q':
-        blinkDash(); delay(600); blinkDash(); delay(600); blinkDot(); delay(600); blinkDash(); delay(600);
-        break;
-      case 'r':
-        blinkDot(); delay(600); blinkDash(); delay(600); blinkDot(); delay(600);
-        break;
-      case 's':
-        blinkDot(); delay(600); blinkDot(); delay(600); blinkDot(); delay(600);
-        break;
-      case 't':
-        blinkDash(); delay(600);
-        break;
-      case 'u':
-        blinkDot(); delay(600); blinkDot(); delay(600); blinkDash(); delay(600);
-        break;
-      case 'v':
-        blinkDot(); delay(600); blinkDot(); delay(600); blinkDot(); delay(600); blinkDash(); delay(600);
-        break;
-      case 'w':
-        blinkDot(); delay(600); blinkDash(); delay(600); blinkDash(); delay(600);
-        break;
-      case 'x':
-        blinkDash(); delay(600); blinkDot(); delay(600); blinkDot(); delay(600); blinkDash(); delay(600);
-        break;
-      case 'y':
-        blinkDash(); delay(600); blinkDot(); delay(600); blinkDash(); delay(600); blinkDash(); delay(600);
-        break;
-      case 'z':
-        blinkDash(); delay(600); blinkDash(); delay(600); blinkDot(); delay(600); blinkDot(); delay(600);
-        break;
-      default:
-        // For unsupported characters, just delay for a word gap
-        delay(2100);
-        break;
-    }
-    delay(300); // Gap between letters
+void resetName() {
+  // Reset the name display here
+  digitalWrite(led1, LOW);
+  delay(1000);
+  digitalWrite(led1, HIGH);
+  delay(1000);
+  digitalWrite(led1, LOW);
+  delay(1000);
+}
+
+void checkButtonPress() {
+  if (digitalRead(buttonPin) == LOW) {
+    isButtonPressed = true;
   }
-  delay(4200); // Gap between words
 }
